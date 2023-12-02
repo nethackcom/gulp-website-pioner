b80fcbc1b57a5c71fd5e4f994680113a446e98f4
nethackcom <forprogrammer@mail.ru>
2023-12-02 20:04:11 +0300

    Update 20:04 2.12.23

--- src/js/modules/mobileNav.js
+++ src/js/modules/mobileNav.js
@@ -1,9 +1,11 @@
 const mobileNav = () => {
     const hamburgerBtn = document.querySelector(".mobile-nav-btn");
     const hamburgerIcon = document.querySelector(".nav-icon");
+    const nav = document.querySelector(".mobile_nav")
 
     hamburgerBtn.addEventListener("click", () => {
         hamburgerIcon.classList.toggle("nav-icon--active");
+        nav.classList.toggle("open");
     });
 }
 
